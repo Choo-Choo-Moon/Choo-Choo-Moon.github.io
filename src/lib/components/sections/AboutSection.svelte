4022d84d44d23e05741080b4e5678eef9f0ac28d
Choo-Choo-Moon <m34821@gmail.com>
2025-11-18 15:45:37 +0900

    logo 경로 변경

--- src/lib/components/sections/AboutSection.svelte
+++ src/lib/components/sections/AboutSection.svelte
@@ -9,7 +9,7 @@
 		<div class="relative h-[600px] w-full overflow-hidden md:h-[1200px] md:w-1/2">
 			<img
 				class="h-full w-full scale-125 object-cover drop-shadow-[10px_8px_6px] drop-shadow-gray-800 md:scale-100"
-				src="/images/main.png"
+				src="/images/logo/main.png"
 				alt="logo"
 			/>
 			<div
@@ -32,19 +32,23 @@
 				<div>
 					<h4 class="mb-1 text-xl font-normal opacity-70">Mobile</h4>
 					<div class="flex flex-wrap gap-10">
-						<Icon src="/images/Kotlin_Logo.svg" alt="Kotlin_Logo" url="https://kotlinlang.org/" />
 						<Icon
-							src="/images/Android_Logo.svg"
+							src="/images/logo/Kotlin_Logo.svg"
+							alt="Kotlin_Logo"
+							url="https://kotlinlang.org/"
+						/>
+						<Icon
+							src="/images/logo/Android_Logo.svg"
 							alt="Android_Logo"
 							url="https://www.android.com/intl/ko_kr/"
 						/>
 						<Icon
-							src="/images/Jetpack_Logo.png"
+							src="/images/logo/Jetpack_Logo.png"
 							alt="Jetpack_Logo"
 							url="https://developer.android.com/jetpack?hl=ko"
 						/>
 						<Icon
-							src="/images/JetpackCompose_Logo.svg"
+							src="/images/logo/JetpackCompose_Logo.svg"
 							alt="JetpackCompose_Logo"
 							url="https://developer.android.com/compose"
 						/>
@@ -54,52 +58,60 @@
 					<h4 class="mb-1 text-xl font-normal opacity-70">Frontend</h4>
 					<div class="flex flex-wrap gap-10">
 						<Icon
-							src="/images/JavaScript_Logo.svg"
+							src="/images/logo/JavaScript_Logo.svg"
 							alt="JavaScript_Logo"
 							url="https://developer.mozilla.org/ko/docs/Web/JavaScript"
 						/>
-						<Icon src="/images/Vue_Logo.svg" alt="Vue_Logo" url="https://ko.vuejs.org/" />
+						<Icon src="/images/logo/Vue_Logo.svg" alt="Vue_Logo" url="https://ko.vuejs.org/" />
 					</div>
 				</div>
 				<div>
 					<h4 class="mb-5 text-xl font-normal opacity-70">Backend</h4>
 					<div class="flex flex-wrap gap-10">
-						<Icon src="/images/Java_Logo.svg" alt="Java_Logo" url="https://www.java.com/ko/" />
-						<Icon src="/images/Spring_Logo.svg" alt="Spring_Logo" url="https://spring.io/" />
-						<Icon src="/images/MySQL_Logo.svg" alt="MySQL_Logo" url="https://www.mysql.com/" />
+						<Icon src="/images/logo/Java_Logo.svg" alt="Java_Logo" url="https://www.java.com/ko/" />
+						<Icon src="/images/logo/Spring_Logo.svg" alt="Spring_Logo" url="https://spring.io/" />
+						<Icon src="/images/logo/MySQL_Logo.svg" alt="MySQL_Logo" url="https://www.mysql.com/" />
 					</div>
 				</div>
 				<div>
 					<h4 class="mb-5 text-xl font-normal opacity-70">Version Control Tool</h4>
 					<div class="flex flex-wrap gap-10">
-						<Icon src="/images/Git_Logo.svg" alt="Git_Logo" url="https://git-scm.com/" />
+						<Icon src="/images/logo/Git_Logo.svg" alt="Git_Logo" url="https://git-scm.com/" />
 						{#if themeStore.getTheme() == 'light'}
-							<Icon src="/images/Github_Logo.svg" alt="Github_Logo" url="https://github.com/" />
+							<Icon
+								src="/images/logo/Github_Logo.svg"
+								alt="Github_Logo"
+								url="https://github.com/"
+							/>
 						{:else}
 							<Icon
-								src="/images/Github_White_Logo.svg"
+								src="/images/logo/Github_White_Logo.svg"
 								alt="Github_White_Logo"
 								url="https://github.com/"
 							/>
 						{/if}
-						<Icon src="/images/GitLab_Logo.svg" alt="GitLab_Logo" url="https://about.gitlab.com/" />
+						<Icon
+							src="/images/logo/GitLab_Logo.svg"
+							alt="GitLab_Logo"
+							url="https://about.gitlab.com/"
+						/>
 					</div>
 				</div>
 				<div>
 					<h4 class="mb-5 text-xl font-normal opacity-70">Collaboration Tool</h4>
 					<div class="flex flex-wrap gap-10">
 						<Icon
-							src="/images/Jira_Logo.svg"
+							src="/images/logo/Jira_Logo.svg"
 							alt="Jira_Logo"
 							url="https://www.atlassian.com/ko/software/jira"
 						/>
 						<Icon
-							src="/images/Figma_Logo.svg"
+							src="/images/logo/Figma_Logo.svg"
 							alt="Figma_Logo"
 							url="https://www.figma.com/ko-kr/"
 						/>
 						<Icon
-							src="/images/Slack_Logo.svg"
+							src="/images/logo/Slack_Logo.svg"
 							alt="Slack_Logo"
 							url="https://slack.com/intl/ko-kr"
 						/>
@@ -110,50 +122,62 @@
 					<h4 class="mb-5 text-xl font-normal opacity-70">Interested</h4>
 					<div class="flex flex-wrap gap-10">
 						<Icon
-							src="/images/ComposeMultiPlatform_Logo.svg"
+							src="/images/logo/ComposeMultiPlatform_Logo.svg"
 							alt="ComposeMultiPlatform_Logo"
 							url="https://www.jetbrains.com/ko-kr/compose-multiplatform/"
 						/>
 						{#if themeStore.getTheme() == 'light'}
-							<Icon src="/images/Quarkus_Logo.svg" alt="Quarkus_Logo" url="https://quarkus.io/" />
+							<Icon
+								src="/images/logo/Quarkus_Logo.svg"
+								alt="Quarkus_Logo"
+								url="https://quarkus.io/"
+							/>
 						{:else}
 							<Icon
-								src="/images/Quarkus_Reverse_Logo.svg"
+								src="/images/logo/Quarkus_Reverse_Logo.svg"
 								alt="Quarkus_Reverse_Logo"
 								url="https://quarkus.io/"
 							/>
 						{/if}
 						<Icon
-							src="/images/GraalVM_Logo.svg"
+							src="/images/logo/GraalVM_Logo.svg"
 							alt="GraalVM_Logo"
 							url="https://www.graalvm.org/"
 						/>
 						<Icon
-							src="/images/PostgresSQL_Logo.svg"
+							src="/images/logo/PostgresSQL_Logo.svg"
 							alt="PostgresSQL_Logo"
 							url="https://www.postgresql.org/"
 						/>
-						<Icon src="/images/Svelte_Logo.svg" alt="Svelte_Logo" url="https://svelte.dev/" />
+						<Icon src="/images/logo/Svelte_Logo.svg" alt="Svelte_Logo" url="https://svelte.dev/" />
 						{#if themeStore.getTheme() == 'light'}
 							<Icon
-								src="/images/Rust_Logo.svg"
+								src="/images/logo/Rust_Logo.svg"
 								alt="Rust_Logo"
 								dropShadowColor={{ r: 150, g: 150, b: 150 }}
 								url="https://rust-lang.org/"
 							/>
 						{:else}
 							<Icon
-								src="/images/Rust_Logo.svg"
+								src="/images/logo/Rust_Logo.svg"
 								alt="Rust_Logo"
 								dropShadowColor={{ r: 255, g: 255, b: 255 }}
 								url="https://rust-lang.org/"
 							/>
 						{/if}
-						<Icon src="/images/Tauri_Logo.svg" alt="Tauri_Logo" url="https://v2.tauri.app/ko/" />
-						<Icon src="/images/Bun_Logo.svg" alt="Bun_Logo" url="https://bun.com/" />
-						<Icon src="/images/Docker_Logo.svg" alt="Docker_Logo" url="https://www.docker.com/" />
-						<Icon src="/images/Zig_Logo.svg" alt="Zig_Logo" url="https://ziglang.org/ko-KR/" />
-						<Icon src="/images/Servo_Logo.svg" alt="Servo_Logo" url="https://servo.org/" />
+						<Icon
+							src="/images/logo/Tauri_Logo.svg"
+							alt="Tauri_Logo"
+							url="https://v2.tauri.app/ko/"
+						/>
+						<Icon src="/images/logo/Bun_Logo.svg" alt="Bun_Logo" url="https://bun.com/" />
+						<Icon
+							src="/images/logo/Docker_Logo.svg"
+							alt="Docker_Logo"
+							url="https://www.docker.com/"
+						/>
+						<Icon src="/images/logo/Zig_Logo.svg" alt="Zig_Logo" url="https://ziglang.org/ko-KR/" />
+						<Icon src="/images/logo/Servo_Logo.svg" alt="Servo_Logo" url="https://servo.org/" />
 					</div>
 				</div>
 			</div>
