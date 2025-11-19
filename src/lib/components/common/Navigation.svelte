60b69bc96392189808aa21cbd43ad2048a7dafe2
Choo-Choo-Moon <m34821@gmail.com>
2025-11-19 20:26:25 +0900

    네비게이션 햄버거모양 추가

--- src/lib/components/common/Navigation.svelte
+++ src/lib/components/common/Navigation.svelte
@@ -1,12 +1,26 @@
-<script>
+<script lang="ts">
 	import ThemeToggle from '$lib/components/common/ThemeToggle.svelte';
+	import type { Menu } from '../../../types';
 	import Icon from './Icon.svelte';
 
 	let { selected, menus = [], onSelected } = $props();
+	let isMenuOpen = $state(false);
+
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen;
+	}
+
+	function handleMenuClick(menu: Menu) {
+		onSelected(menu);
+		isMenuOpen = false;
+	}
 </script>
 
-<div class="fixed top-0 z-50 flex h-17 w-full flex-nowrap items-center justify-end px-4">
-	<div class="flex flex-nowrap">
+<div
+	class="fixed top-0 z-50 flex h-17 w-full flex-row-reverse flex-nowrap items-center px-4 md:flex-row md:justify-end"
+>
+	<!-- Desktop Menu -->
+	<div class="hidden flex-nowrap md:flex">
 		{#each menus as menu (menu.id)}
 			<button
 				type="button"
@@ -34,5 +48,60 @@
 			</button>
 		</a>
 	</div>
+
+	<!-- Mobile Hamburger Button -->
+	<button
+		type="button"
+		class="flex cursor-pointer flex-col gap-1.5 border-0 p-2 md:hidden"
+		onclick={toggleMenu}
+		aria-label="Toggle menu"
+	>
+		<span
+			class="block h-0.5 w-6 bg-black transition-all dark:bg-white"
+			class:rotate-45={isMenuOpen}
+			class:translate-y-2={isMenuOpen}
+		></span>
+		<span class="block h-0.5 w-6 bg-black transition-all dark:bg-white" class:opacity-0={isMenuOpen}
+		></span>
+		<span
+			class="block h-0.5 w-6 bg-black transition-all dark:bg-white"
+			class:-rotate-45={isMenuOpen}
+			class:-translate-y-2={isMenuOpen}
+		></span>
+	</button>
+
 	<ThemeToggle />
 </div>
+
+<!-- Mobile Menu Dropdown -->
+{#if isMenuOpen}
+	<div class="fixed top-17 left-0 z-40 w-full bg-white shadow-lg md:hidden dark:bg-black">
+		<div class="flex flex-col">
+			{#each menus as menu (menu.id)}
+				<button
+					type="button"
+					class="cursor-pointer border-0 px-4 py-4 text-left transition-colors hover:bg-gray-100 dark:hover:bg-gray-800"
+					class:bg-gray-100={menu.id === selected.id}
+					class:dark:bg-gray-800={menu.id === selected.id}
+					onclick={() => handleMenuClick(menu)}
+				>
+					<p class="font-bold">{menu.label}</p>
+				</button>
+			{/each}
+			<a href="https://velog.io/@charlie_moon/posts" target="_blank" class="border-0">
+				<button
+					type="button"
+					class="flex w-full cursor-pointer items-center gap-2 border-0 px-4 py-4 text-left transition-colors hover:bg-gray-100 dark:hover:bg-gray-800"
+				>
+					<p class="font-bold">Blog</p>
+					<Icon
+						src="/images/icon/New_Window_Icon_Light.svg"
+						darkSrc="/images/icon/New_Window_Icon_Dark.svg"
+						alt="open blog on new window icon"
+						size="size-5"
+					/>
+				</button>
+			</a>
+		</div>
+	</div>
+{/if}
