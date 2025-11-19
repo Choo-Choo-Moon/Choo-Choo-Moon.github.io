67ff28f32b907fb1dfe5de73e9fae8c63faeec9c
Choo-Choo-Moon <m34821@gmail.com>
2025-11-19 20:25:45 +0900

    hello 섹션 레이아웃 수정

--- src/lib/components/sections/HelloSection.svelte
+++ src/lib/components/sections/HelloSection.svelte
@@ -3,18 +3,20 @@
 	let isDarkMode = $derived(themeStore.theme === 'dark');
 </script>
 
-<section id="hello" class="min-h-screen pt-17 overflow-x-hidden">
-	<div class="relative w-full min-h-[600px]">
+<section id="hello" class="min-h-screen overflow-x-hidden pt-17">
+	<div
+		class=" flex min-h-[600px] w-full flex-col items-center justify-center gap-10 md:h-screen md:flex-row md:justify-between"
+	>
 		<!-- Hello World Text -->
 		<h1
-			class="absolute z-10 left-2 top-20 sm:left-10 sm:top-30 inline-block rotate-315 transform text-[clamp(2rem,12vw,6rem)] sm:text-[clamp(3rem,15vw,8rem)] md:text-[clamp(4rem,18vw,10rem)]"
+			class=" inset-0 top-20 z-10 inline-block rotate-315 transform text-[clamp(2rem,18vw,6rem)] sm:top-30 sm:left-10 sm:text-[clamp(3rem,15vw,8rem)] md:mr-10 md:ml-20 md:text-[clamp(4rem,18vw,10rem)]"
 		>
 			Hello! <br /> &nbsp;&nbsp;&nbsp;&nbsp; World!
 		</h1>
 
 		<!-- Logo Image -->
 		<img
-			class="absolute top-32 sm:top-40 -right-10 sm:right-5 md:right-10 lg:right-20 z-0 size-[clamp(150px,50vw,250px)] sm:size-[clamp(180px,40vw,300px)] md:size-[clamp(250px,35vw,400px)] lg:size-[clamp(300px,30vw,500px)] animate-spin [animation-duration:10s]"
+			class=" top-32 z-0 size-[clamp(150px,50vw,250px)] animate-spin [animation-duration:10s] sm:top-40 sm:right-5 sm:size-[clamp(180px,40vw,300px)] md:right-10 md:mr-100 md:size-[clamp(250px,35vw,400px)] lg:right-20 lg:size-[clamp(300px,30vw,500px)]"
 			src={isDarkMode ? '/images/logo/Main_Logo_Light.svg' : '/images/logo/Main_Logo_Dark.svg'}
 			alt="spin main logo"
 		/>
